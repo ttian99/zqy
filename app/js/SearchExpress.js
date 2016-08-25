7cba63dc6300f45bcaaac5987678deaadbd4c380
ttian99 <176553611@qq.com>
2016-08-25 10:30:37 +0800

    完善查询功能

--- app/js/SearchExpress.js
+++ app/js/SearchExpress.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Container, Group, Slider, Field, Icon, Button, Grid, List } from 'amazeui-touch';
+import  Fetch from './Fetch';
+import moment from 'moment';
 
 class SearchExpress extends React.Component {
   constructor(props){
@@ -18,20 +20,52 @@ class SearchExpress extends React.Component {
   // 挂载完成后立刻调用
   componentDidMount = () => {} 
 
+  handlerClick = () => {
+    console.log('-------- click --------');
+    const searchValue = this.refs.input.getValue();
+    console.log("-- searchValue -- "+ searchValue);
+
+    const obj = {
+      cmd: 'search-info',
+      words: searchValue
+    }
+    Fetch(obj, (err, json) => {
+      if(err) console.log(err)
+      console.log('------- back ----')
+      console.log(json);
+      this.setDataList(json.data);
+    });
+  }
+
+  setDataList = (arr) => {
+    console.log('-------- setDataList -------');
+    console.log(arr);
+    this.setState({ dataList: arr });
+  }
+
   render() {
+    const btn = (
+      <Button onClick={this.handlerClick.bind(this)}>查询</Button>
+    );
     return (
       <Container scrollable={true}>
         <Field
+          ref="input"
           placeholder="请输入关键词..."
           labelBefore={<Icon name="search" />}
-          btnAfter={<Button>查询</Button>}
+          btnAfter={btn}
         />
         <Grid>
         <List className="search-express-list" >
           {
-            this.props.dataList.map((value, i) => {
+            this.state.dataList.map((value, i) => {
+              const date = moment(value.time).format('YYYY-MM-DD');
               return(
-                <List.Item key={i}>{value}</List.Item>
+                <List.Item key={i}>
+                {
+                  date + " | " + value.goodsName + " | " + value.goodsNum + " | " + value.goodsMod + " | " + value.mfr + " | " + value.desc + " | " + value.epsCnee + " | " + value.trueCnee
+                }
+                </List.Item>
               )
             })
           }
